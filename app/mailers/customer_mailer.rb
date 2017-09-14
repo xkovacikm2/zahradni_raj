67277c12d2fa428cf4228c4c702bbb859fbe088e
Michal Kovacik <xkovacikm2@gmail.com>
2017-09-14 18:07:33 +0200

    Mailer YOLO

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -1,8 +1,10 @@
 class CustomerMailer < ActionMailer::Base
   default from: 'info@zahradniraj.cz'
 
-  def mass_emails(users = nil, content = '', subject = '')
-    @content = content
-    mail(to: 'xkovacikm2@gmail.com', subject: subject)
+  def mass_emails(customers = [], content = '', subject = '')
+    customers.each do |customer|
+      @content = content
+      mail(to: customer.email, subject: subject)
+    end
   end
 end
